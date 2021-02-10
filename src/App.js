400e356ebb162d66fcd378f96684e10b0f844320
Chandra Sekhar <chinnachandu21@gmail.com>
2021-02-10 11:48:39 +0530

    checking commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Samplefetch from '../src/components/samplefetch/Samplefetch';
+import RichTextEditor from '../src/components/richtexteditor/RichTextEditor';
 
 function App() {
   return (
     <div className="App">
      <Samplefetch/>
+     <RichTextEditor/>
      
     </div>
   );
---
+++ src/components/richtexteditor/RichTextEditor.jsx
@@ -0,0 +1,22 @@
+import React, {Component, Fragment} from 'react';
+
+class RichTextEditor extends Component {
+    constructor(props){
+     super(props);
+    
+    }
+     componentDidMount(){
+    
+    }
+
+    render(){
+        return(
+            <div>
+                
+               RichTextEditor
+            </div>
+        )
+    }
+}
+
+export default RichTextEditor
